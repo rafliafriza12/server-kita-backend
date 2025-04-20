e3aef04562e7fc7a369d4ae661d2a3d5439b3c9f
rafliafriza12 <rafliafriza90@gmail.com>
2025-04-21 06:18:46 +0700

    feat: deploy project

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -17,6 +17,7 @@ authRouter.post("/login", login);
 authRouter.post("/register", register);
 authRouter.post("/logout", logout);
 authRouter.put("/editProfile/:userId", editProfile);
+authRouter.put("/editProfile", editProfile);
 authRouter.put("/changePassword", changePassword);
 authRouter.get("/getUserProfile", getUserProfile);
 authRouter.get("/getAllUsers", getAllUsers);
